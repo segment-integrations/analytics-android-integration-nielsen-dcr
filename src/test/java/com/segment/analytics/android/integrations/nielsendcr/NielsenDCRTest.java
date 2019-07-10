17faba4467339ea73bcfaa930b36859ead1047ba
Carlos Mecha <mecha.carlos@gmail.com>
2019-07-10 13:59:51 -0700

    Update build and tests

--- src/test/java/com/segment/analytics/android/integrations/nielsendcr/NielsenDCRTest.java
+++ src/test/java/com/segment/analytics/android/integrations/nielsendcr/NielsenDCRTest.java
@@ -1,21 +1,19 @@
 package com.segment.analytics.android.integrations.nielsendcr;
 
 
-import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.mockito.Matchers.argThat;
-
 import static org.mockito.Mockito.validateMockitoUsage;
 import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
-import android.content.pm.PackageManager;
+
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import com.nielsen.app.sdk.AppSdk;
 import com.segment.analytics.Analytics;
-import com.segment.analytics.Options;
 
 import com.segment.analytics.Properties;
 import com.segment.analytics.integrations.Logger;
-import com.segment.analytics.test.ScreenPayloadBuilder;
-import com.segment.analytics.test.TrackPayloadBuilder;
+import com.segment.analytics.integrations.ScreenPayload;
+import com.segment.analytics.integrations.TrackPayload;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -24,38 +22,39 @@ import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 18, manifest = Config.NONE)
-
-@PrepareForTest(AppSdk.class) public class NielsenDCRTest {
+@Config(manifest = Config.NONE)
+public class NielsenDCRTest {
 
   @Mock AppSdk nielsen;
-  @Mock Analytics analytics;
   private Logger logger;
   private NielsenDCRIntegration integration;
 
-  @Before public void setUp() throws PackageManager.NameNotFoundException {
-    initMocks(this);
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
 
     logger = Logger.with(Analytics.LogLevel.DEBUG);
-
     integration = new NielsenDCRIntegration(nielsen, logger);
   }
 
-  @After public void validate() {
+  @After
+  public void validate() {
     validateMockitoUsage();
   }
 
-  @Test public void videoPlaybackStarted() throws JSONException {
+  @Test
+  public void videoPlaybackStarted() throws JSONException {
     integration.track(
-        new TrackPayloadBuilder().event("Video Playback Started").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Playback Started").properties(new Properties() //
             .putValue("assetId", 1234)
             .putValue("adType", "pre-roll")
             .putValue("totalLength", 120)
@@ -71,12 +70,13 @@ import org.robolectric.annotation.Config;
     verify(nielsen).play(jsonEq(expected));
   }
 
-  @Test public void videoPlaybackPaused() {
+  @Test
+  public void videoPlaybackPaused() {
 
     Map<String, Object> nielsenOptions = new LinkedHashMap<>();
     nielsenOptions.put("channelName", "exampleChannelName");
 
-    integration.track(new TrackPayloadBuilder() //
+    integration.track(new TrackPayload.Builder().anonymousId("foo") //
         .event("Video Playback Paused")
         .properties(new Properties() //
             .putValue("assetId", 1234)
@@ -87,16 +87,17 @@ import org.robolectric.annotation.Config;
             .putValue("fullScreen", true)
             .putValue("bitrate", 50)
             .putValue("sound", 80))
-        .options(new Options().setIntegrationOptions("nielsen-dcr", nielsenOptions))
+        .integration("nielsen-dcr", nielsenOptions)
         .build());
 
     verify(nielsen).stop();
   }
 
-  @Test public void videoPlaybackResumed() throws JSONException {
+  @Test
+  public void videoPlaybackResumed() throws JSONException {
 
     integration.track(
-        new TrackPayloadBuilder().event("Video Playback Resumed").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Playback Resumed").properties(new Properties() //
             .putValue("assetId", 5332)
             .putValue("adType", "post-roll")
             .putValue("totalLength", 100)
@@ -113,13 +114,14 @@ import org.robolectric.annotation.Config;
     verify(nielsen).play(jsonEq(expected));
   }
 
-  @Test public void videoContentStarted() throws JSONException {
+  @Test
+  public void videoContentStarted() throws JSONException {
 
     Map<String, Object> nielsenOptions = new LinkedHashMap<>();
-    nielsenOptions.put("segA", "segmentA");
+    nielsenOptions.put("segB", "segmentB");
 
     integration.track(
-        new TrackPayloadBuilder().event("Video Content Started").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Content Started").properties(new Properties() //
             .putValue("assetId", 123214)
             .putValue("title", "Look Who's Purging Now")
             .putValue("season", 2)
@@ -131,22 +133,27 @@ import org.robolectric.annotation.Config;
             .putValue("fullEpisode", true)
             .putValue("podId", "segment A")
             .putValue("playbackPosition", 70))
-        .options(new Options().setIntegrationOptions("nielsen-dcr", nielsenOptions))
+        .integration("nielsen-dcr", nielsenOptions)
         .build());
 
     JSONObject expected = new JSONObject();
     expected.put("assetid", "123214");
     expected.put("title", "Look Who's Purging Now");
     expected.put("program", "Rick and Morty");
-    expected.put("segA", "segmentA");
+    expected.put("segB", "segmentB");
+    expected.put("pipmode", "false");
+    expected.put("isfullepisode", "y");
+    expected.put("type", "content");
+    expected.put("adloadtype", "1");
 
     verify(nielsen).loadMetadata(jsonEq(expected));
   }
 
-  @Test public void videoAdStarted() throws JSONException {
+  @Test
+  public void videoAdStarted() throws JSONException {
 
     integration.track(
-        new TrackPayloadBuilder().event("Video Ad Started").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Ad Started").properties(new Properties() //
             .putValue("assetId", 4311)
             .putValue("podId", "adSegmentA")
             .putValue("type", "mid-roll")
@@ -162,10 +169,11 @@ import org.robolectric.annotation.Config;
     verify(nielsen).loadMetadata(jsonEq(expected));
   }
 
-  @Test public void videoAdStartedWithTypeMidRoll() throws JSONException {
+  @Test
+  public void videoAdStartedWithTypeMidRoll() throws JSONException {
 
     integration.track(
-        new TrackPayloadBuilder().event("Video Ad Started").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Ad Started").properties(new Properties() //
             .putValue("assetId", 4311)
             .putValue("podId", "adSegmentA")
             .putValue("type", "mid-roll")
@@ -177,17 +185,18 @@ import org.robolectric.annotation.Config;
             .putValue("title", "Helmet Ad")).build());
 
     JSONObject adExpected = new JSONObject();
-    adExpected.put("assetid", "1234");
+    adExpected.put("assetid", "4311");
     adExpected.put("type", "mid-roll");
     adExpected.put("title", "Helmet Ad");
 
     verify(nielsen).loadMetadata(jsonEq(adExpected));
   }
 
-  @Test public void videoAdCompleted() {
+  @Test
+  public void videoAdCompleted() {
 
     integration.track(
-        new TrackPayloadBuilder().event("Video Ad Completed").properties(new Properties() //
+        new TrackPayload.Builder().anonymousId("foo").event("Video Ad Completed").properties(new Properties() //
             .putValue("assetId", 3425)
             .putValue("podId", "adSegmentb")
             .putValue("type", "mid-roll")
@@ -198,14 +207,11 @@ import org.robolectric.annotation.Config;
     verify(nielsen).stop();
   }
 
-  public static JSONObject jsonEq(JSONObject expected) {
-    return argThat(samePropertyValuesAs(expected));
-  }
-
-  @Test public void screen() throws JSONException {
+  @Test
+  public void screen() throws JSONException {
 
     integration.screen(
-        new ScreenPayloadBuilder().name("Home").properties(new Properties() //
+            new ScreenPayload.Builder().anonymousId("foo").name("Home").properties(new Properties() //
             .putValue("variation", "blue sign up button"))
             .build());
     JSONObject expected = new JSONObject();
@@ -216,7 +222,9 @@ import org.robolectric.annotation.Config;
 
     verify(nielsen).loadMetadata(jsonEq(expected));
   }
-  @Test public void screenWithOptions() throws JSONException {
+
+  @Test
+  public void screenWithOptions() throws JSONException {
 
     Map<String, Object> nielsenOptions = new LinkedHashMap<>();
     nielsenOptions.put("segB", "segmentB");
@@ -224,10 +232,9 @@ import org.robolectric.annotation.Config;
     nielsenOptions.put("crossId1", "crossIdValue");
 
     integration.screen(
-        new ScreenPayloadBuilder().name("Home").properties(new Properties() //
+        new ScreenPayload.Builder().anonymousId("foo").name("Home").properties(new Properties() //
             .putValue("variation", "blue sign up button"))
-            .options(new Options()
-            .setIntegrationOptions("nielsen-dcr", nielsenOptions))
+            .integration("nielsen-dcr", nielsenOptions)
             .build());
 
     JSONObject expected = new JSONObject();
@@ -238,5 +245,32 @@ import org.robolectric.annotation.Config;
     expected.put("crossId1", "crossIdValue");
 
     verify(nielsen).loadMetadata(jsonEq(expected));
+  }
+
+  /**
+   * Uses the string representation of the object. Useful for JSON objects.
+   * @param expected Expected object
+   * @return Argument matcher.
+   */
+  private JSONObject jsonEq(JSONObject expected) {
+    return argThat(new JSONMatcher(expected));
+  }
+
+  class JSONMatcher implements ArgumentMatcher<JSONObject> {
+    JSONObject expected;
+
+    JSONMatcher(JSONObject expected) {
+      this.expected = expected;
+    }
+
+    @Override
+    public boolean matches(JSONObject argument) {
+      try {
+        JSONAssert.assertEquals(expected, argument, JSONCompareMode.STRICT);
+        return true;
+      } catch (JSONException e) {
+        return false;
+      }
+    }
   }
 }
